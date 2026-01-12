e27413ecc25da1682b546dfd3f4b809f6be9d0dc
2975645S <2975645S@student.gla.ac.uk>
2026-01-12 23:10:00 +0000

    feat(4): exercises

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 
-def index(request: HttpRequest) -> HttpResponse:
+def index(request: HttpRequest):
     return render(request, "rango/index.html", context={ "boldmessage": "Crunchy, creamy, cookie, candy, cupcake!" })
 
-def about(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(
-"""Rango says here is the about page. <br/>
-<a href="/rango/">Index</a>"""
-    )
+def about(request: HttpRequest):
+    return render(request, "rango/about.html", context={ "boldmessage": "This tutorial has been put together by Jacob." })
