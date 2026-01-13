6946573690a960dc15610ce3e2b95872cb6a83f1
2975645S <2975645S@student.gla.ac.uk>
2026-01-13 00:16:19 +0000

    feat(7): textbook

---
+++ rango/forms.py
@@ -0,0 +1,33 @@
+from django.forms import ModelForm, CharField, IntegerField, HiddenInput
+from rango.models import Category, Page
+
+
+class CategoryForm(ModelForm):
+    name = CharField(max_length=128, help_text="Please enter the category name.")
+    views = IntegerField(widget=HiddenInput(), initial=0)
+    likes = IntegerField(widget=HiddenInput(), initial=0)
+    slug = CharField(widget=HiddenInput(), required=False)
+
+    class Meta:
+        model = Category
+        fields = ("name",)
+
+
+class PageForm(ModelForm):
+    title = CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = CharField(max_length=200, help_text="Please enter the URL of the page.")
+    views = IntegerField(widget=HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+        exclude = ("category",)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get("url")
+
+        if url and not url.startswith("http://"):
+            url = f"http://{url}"
+            cleaned_data["url"] = url
+
+        return cleaned_data
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 
 def index(request: HttpRequest):
@@ -8,14 +9,14 @@ def index(request: HttpRequest):
     context["categories"] = Category.objects.order_by("-likes")[:5]  # top 5 categories
     context["pages"] = Page.objects.order_by("-views")[:5]  # top 5 pages
 
-    return render(request, "rango/index.html", context=context)
+    return render(request, "rango/index.html", context)
 
 
 def about(request: HttpRequest):
     return render(
         request,
         "rango/about.html",
-        context={"boldmessage": "This tutorial has been put together by Jacob."},
+        {"boldmessage": "This tutorial has been put together by Jacob."},
     )
 
 
@@ -31,4 +32,19 @@ def show_category(request: HttpRequest, category_name_slug: str):
         context["category"] = None
         context["pages"] = None
 
-    return render(request, "rango/category.html", context=context)
+    return render(request, "rango/category.html", context)
+
+
+def add_category(request: HttpRequest):
+    form = CategoryForm()
+
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, "rango/add_category.html", {"form": form})
